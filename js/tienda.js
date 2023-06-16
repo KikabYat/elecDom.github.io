5baaf63be6520a6510cab89e5fe0bf3b1f0e2432
KikabYat <35947230+KikabYat@users.noreply.github.com>
2023-06-15 18:59:43 -0600

    Update tienda.js

--- js/tienda.js
+++ js/tienda.js
@@ -12,37 +12,37 @@ function muestraBombillos(){
     seccionProductos.innerHTML='';
     seccionProductos.innerHTML=`
 <div class="producto">
-<figure><img src="/images/taladro.jfif" alt=""></figure>
+<figure><img src="../images/taladro.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Pala</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/taladro de pared.jfif" alt=""></figure>
+<figure><img src="../images/taladro de pared.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/BombillaColocha.png" alt=""></figure>
+<figure><img src="../images/BombillaColocha.png" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/plafonera1.jpg" alt=""></figure>
+<figure><img src="../images/plafonera1.jpg" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/obrero.jpg" alt=""></figure>
+<figure><img src="../images/obrero.jpg" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/chaleco reflectivo.jpg" alt=""></figure>
+<figure><img src="../images/chaleco reflectivo.jpg" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
@@ -64,31 +64,31 @@ function muestraProductosLiquidacion(){
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta electrica.jfif" alt=""></figure>
+<figure><img src="../images/herramienta electrica.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta1.jfif" alt=""></figure>
+<figure><img src="../images/herramienta1.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta2.jfif" alt=""></figure>
+<figure><img src="../images/herramienta2.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta3.jfif" alt=""></figure>
+<figure><img src="../images/herramienta3.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta4.jfif" alt=""></figure>
+<figure><img src="../images/herramienta4.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
@@ -102,37 +102,37 @@ function muestraProductosNuevoIngreso(){
     seccionProductos.innerHTML='';
     seccionProductos.innerHTML=`
 <div class="producto">
-<figure><img src="/images/250x300/240x300 otra.jpg" alt=""></figure>
+<figure><img src="../images/250x300/240x300 otra.jpg" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Piocha</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta1.jfif" alt=""></figure>
+<figure><img src="../images/herramienta1.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta6.jfif" alt=""></figure>
+<figure><img src="../images/herramienta6.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta4.jfif" alt=""></figure>
+<figure><img src="../images/herramienta4.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/herramienta3.jfif" alt=""></figure>
+<figure><img src="../images/herramienta3.jfif" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
 </div>
 <div class="producto">
-<figure><img src="/images/250x300/240x300 otra.jpg" alt=""></figure>
+<figure><img src="../images/250x300/240x300 otra.jpg" alt=""></figure>
 <p class="descripcion-producto">Descripcion: Bombilla LED 18W</p>
 <P class="precio-producto">Precio: Q25.00</p>
 <button><i class="fa-solid fa-cart-plus"></i></button>
